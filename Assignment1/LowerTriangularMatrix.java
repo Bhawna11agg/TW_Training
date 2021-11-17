bf59c5a0b9bd9a6307a989978949cec29e87d9ee
Bhawna11agg <47032273+Bhawna11agg@users.noreply.github.com>
2021-11-17 23:21:48 +0530

    Create Lower Triangular Matrix Program

---
+++ Assignment1/LowerTriangularMatrix.java
@@ -0,0 +1,40 @@
+package com.tw.assignment1;
+
+import java.util.Scanner;
+
+public class LowerTriangularMatrix {
+    public static void main(String[] args) {
+
+        int row, col;
+        Scanner sc = new Scanner(System.in);
+        row = sc.nextInt();
+        col = sc.nextInt();
+
+        int[][] matrix = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = i + 1; j < col; j++) {
+                if (matrix[i][j] != 0) {
+                    System.out.println("no");
+                    return;
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (matrix[i][j] == 0) {
+                    System.out.println("no");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("yes");
+    }
+}
